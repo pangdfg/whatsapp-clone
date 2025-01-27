3c5a5cf0061346d812e7759e7c2c7899c7da4480
poiu <ponpottboomk@gmail.com>
2025-01-27 15:05:45 +0700

    Init spingboot

---
+++ whatsappclone/src/main/java/com/whatsapp/whatsappclone/WhatsAppCloneApplication.java
@@ -0,0 +1,13 @@
+package com.whatsapp.whatsappclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsAppCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsAppCloneApplication.class, args);
+	}
+
+}
